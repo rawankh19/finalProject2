b6787437314b9b84c5592f5ff577872797798158
rawankh19 <rawanbolbol8@gmail.com>
2021-06-05 00:45:16 +0300

    rawan02

---
+++ app/src/test/java/com/example/finalproject/ExampleUnitTest.java
@@ -0,0 +1,8 @@
+package com.example.finalproject;
+
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
